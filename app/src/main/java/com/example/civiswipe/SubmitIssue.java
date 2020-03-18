889550d5f7500b61c2591bfc2347108478bc52b9
meredith <listermeredith@gmail.com>
2020-03-18 16:36:43 -0700

    test commit & created new activity

---
+++ app/src/main/java/com/example/civiswipe/SubmitIssue.java
@@ -0,0 +1,14 @@
+package com.example.civiswipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubmitIssue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit_issue);
+    }
+}
