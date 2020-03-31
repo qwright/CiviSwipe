51c06086b974679432cc9ebdbfc59d7c4b141816
samCan95 <samCan95@users.noreply.github.com>
2020-03-30 20:42:40 -0700

    Commenting thread's back button has been fixed

--- app/src/main/java/com/example/civiswipe/MainActivity.java
+++ app/src/main/java/com/example/civiswipe/MainActivity.java
@@ -45,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v){
                 Intent intent =new Intent(MainActivity.this, commentThread.class);
                 startActivity(intent);
-                finish();
 
             }
 
